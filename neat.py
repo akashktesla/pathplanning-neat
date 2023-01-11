1ce0e7c51e59ea9d1bb8e5c12122344777fd0227
akashktesla <akashktesla@gmail.com>
2023-01-11 13:11:26 +0530

    :)

--- neat.py
+++ neat.py
@@ -87,19 +87,19 @@ def sigmoid(x):
 def relu(x):
 	return max(0.0, x)
 
-def network_io_string(nn):
+def network_to_string(nn):
     returns = set()
-    for i in nn:
+    for i in nn.c_network:
         returns.add(f"{i.input}-{i.output}")
     return returns
 
 def crossover(a,b):
-    aset = network_io_string(a.input_network)
-    bset = network_io_string(b.input_network)
+    aset = network_to_string(a)
+    bset = network_to_string(b)
     node_hm = {}
-    for i in a.input_network:
+    for i in a.c_network:
         node_hm[i.to_key()] = i.to_string()
-    for i in b.input_network:
+    for i in b.c_network:
         node_hm[i.to_key()] = i.to_string()
     asi = aset.union(bset)
     print(f'node-hashmap: {node_hm}')
@@ -154,6 +154,7 @@ def mutation(nn):
     mutation_c(nn,possible_connections)
     mutation_an(nn)
     mutation_cw(nn)
+    mutation_dn(nn)
     nn.print()
     #adding new node
     #modifing exsisting weights sooo... mayb like with step size or some shit idk
@@ -210,13 +211,51 @@ def mutation_dn(nn):
     random_node.is_disabled = True
 
 def calculate(nn):
-    nn.print()
+    # nn.print()
     for i in nn.c_network:
         in_val = nn.node_map[str(i.input)]
         val = i.calculate(in_val)
         nn.node_map[str(i.output)] = val
-    print("valzkai ae")
     nn.print()
+    for i in nn.output_layer:
+        print(f"output at node:{i} is {nn.node_map[str(i)]}")
+
+def crossover_test():
+    parent1 = NeuralNetwork(['1','2','3','4'], #input network
+                       ['8','9'], #output network 
+                       {"1":2.3,"2":3.7,"3":3.3,"4":4.1,"5":0,"6":0,"7":0,"8":0,"9":0}, # node_map 
+                       [Connect(1,1,5,1.2),Connect(2,1,6,3.2),Connect(3,1,7,3.2),
+                        Connect(4,2,5,5.2),Connect(5,2,6,4.5),
+                        Connect(6,3,5,2.3),
+                        Connect(8,4,6,8.3),
+                        Connect(9,5,8,4.3),
+                        Connect(10,6,8,9.3),
+                        Connect(12,7,8,7.2),Connect(13,7,9,4.2)],
+                       2.5,#weights stepsize
+                       13, #next innovation number
+                       10, #next node number
+                       )
+    parent2 = NeuralNetwork(['1','2','3','4'], #input network
+                       ['8','9'], #output network 
+                       {"1":2.3,"2":3.7,"3":3.3,"4":4.1,"5":0,"6":0,"7":0,"8":0,"9":0}, # node_map 
+                       [Connect(1,1,5,1.2),Connect(2,1,6,3.2),Connect(3,1,7,3.2),
+                        Connect(4,2,5,5.2),Connect(5,2,6,4.5),
+                        Connect(6,3,5,2.3),Connect(7,3,7,1.2),
+                        Connect(8,4,6,8.3),
+                        Connect(9,5,8,4.3),
+                        Connect(10,6,8,9.3),Connect(11,6,9,3.7),
+                        Connect(12,7,8,7.2),Connect(13,7,9,4.2)],
+                       2.5,#weights stepsize
+                       13, #next innovation number
+                       10, #next node number
+                       )
+    offspring = crossover(parent1,parent2)
+    print(offspring)
+    for i in offspring:
+            i.print()
+
+
+
 
 def main():
     nn = NeuralNetwork(['1','2','3','4'], #input network
@@ -228,11 +267,14 @@ def main():
                         Connect(8,4,6,8.3),
                         Connect(9,5,8,4.3),
                         Connect(10,6,8,9.3),Connect(11,6,9,3.7),
-                        Connect(12,7,8,7.2),Connect(13,7,9,7.2)],
+                        Connect(12,7,8,7.2),Connect(13,7,9,4.2)],
                        2.5,#weights stepsize
                        13, #next innovation number
                        10, #next node number
                        )
+
+    # calculate(nn)
+    # crossover_test()
     mutation(nn)
 
 if __name__ == "__main__":
