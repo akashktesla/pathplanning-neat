3faaa2f3da8618982fc6169471cf223382c215c8
akashktesla <akashktesla@gmail.com>
2023-02-07 18:10:06 +0530

    :)

--- neat_training.py
+++ neat_training.py
@@ -34,7 +34,7 @@ class Enemy(Entity):
         pass
 
 class Agent(Entity):
-    def __init__(self,_index,nn,model,pos):
+    def __init__(self,_index,nn,model,pos,target):
         super().__init__()
         self.pop_index = _index
         self.model = model
@@ -46,7 +46,10 @@ class Agent(Entity):
         self.displacement = 0
         self.fitness = 0
         self.is_alive=True
-
+        # target pos to neural network
+        self.nn.node_map[13] = target[0]
+        self.nn.node_map[14] = target[1]
+        self.nn.node_map[15] = target[2]
     def input(self,key):
         #input for agent
         pass
@@ -55,6 +58,14 @@ class Agent(Entity):
         # print(f"id: {self.pop_index}")
         # print(f"alive: {self.is_alive}")
         if self.is_alive:
+            # position to neural network
+            pos = self.position
+            print(f"possss: {pos}")
+            self.nn.node_map[10] = pos[0]
+            self.nn.node_map[11] = pos[1]
+            self.nn.node_map[12] = pos[2]
+
+
             rot = self.rotation_y
             self.rotation_y = rot
             angle1 = (rot)*math.pi/180
@@ -149,62 +160,6 @@ class Agent(Entity):
         #updates fitness according to the parameters
         pass 
 
-class drone(Entity):
-    def __init__(self,model,position):
-        super().__init__()
-        self.model="cube"
-        self.position = position
-        self.texture = "white_cube"
-    def input(self,key):
-        global render_displacement
-        disp = math.dist((0,0,0),(self.x,self.y,self.z))
-        if key=="z":
-            for i in render_displacement:
-                destroy(i)
-            render_displacement.append(Text(text = f"Displacement:{disp}",y = 0.5,x = -.85,z = -1))
-        pass
-
-    def update(self):
-        global td
-        global tof_const
-        global tof
-        tof = int(time.time()-tof_const)
-        global render
-        for i in render:
-            destroy(i)
-        render.append(Text(text = f"Total distance travelled: {td}",y = 0.40,x = -.85,z = -1))
-        render.append(Text(text = f"Time of flight: {tof}",y = 0.45,x = -.85,z = -1))
-        global speed
-        #all 4 angles used to calculate all the 4 movements
-        angle1 = (self.rotation.y)*math.pi/180
-        angle2 = (self.rotation.y+90)*math.pi/180
-        angle3 = (self.rotation.y+180)*math.pi/180
-        angle4 = (self.rotation.y+270)*math.pi/180
-        #movement script
-        if held_keys["up arrow"] or held_keys["w"]:
-            self.x += time.dt*speed*math.sin(angle1)
-            self.z += time.dt*speed*math.cos(angle1)
-            td += mod(time.dt*speed*math.sin(angle1)) + mod(time.dt*speed*math.cos(angle1))
-        if held_keys["right arrow"] or held_keys["d"]:
-            self.x += time.dt*speed*math.sin(angle2)
-            self.z += time.dt*speed*math.cos(angle2)
-            td += mod(time.dt*speed*math.sin(angle2)) + mod(time.dt*speed*math.cos(angle2))
-        if held_keys["down arrow"] or held_keys["s"]:
-            self.x += time.dt*speed*math.sin(angle3)
-            self.z += time.dt*speed*math.cos(angle3)
-            td += mod(time.dt*speed*math.sin(angle3)) + mod(time.dt*speed*math.cos(angle3))
-        if held_keys["left arrow"] or held_keys["a"]:
-            self.x += time.dt*speed*math.sin(angle4)
-            self.z += time.dt*speed*math.cos(angle4)
-            td += mod(time.dt*speed*math.sin(angle4)) + mod(time.dt*speed*math.cos(angle4))
-
-
-class simobj(Entity):
-    def __init__(self,model,position ):
-        super().__init__()
-        self.model=model
-        self.position = position
-
 def input(key):
     if key=="d":
         terrain_degeneration()
@@ -251,6 +206,7 @@ def population_degeneration():
         destroy(i)
 
 def neat_training():
+    global generation_number
     #10,11,12 - player pos
     #13,14,15 - target pos
     nn = NeuralNetwork(
@@ -272,30 +228,20 @@ def neat_training():
     global population
     population = []
     for i,item in enumerate(initial_population):
-        population.append(Agent(i,item,"cube",(0,0.5,0)))
+        population.append(Agent(i,item,"cube",(0,0.5,0),(0,0,100)))
 
 def main():
     app = Ursina()
-    global td
-    td = 0
-    global tof_const
-    tof_const = time.time()
-    global render_displacement
-    render_displacement = []
     global vcamera
+    global generation_number
+    generation_number = 1
     window.exit_button.visible = False
     window.fps_counter.enabled = True
     window.title = "Simulation"
     vcamera = EditorCamera()
-    global y
-    global render 
-    render = []
     Entity(model='plane', scale=1000, color=color.white, shader=lit_with_shadows_shader)
-    global entities
-    global aki
     global speed
     speed = 20
-    entities = []
     global terrain
     terrain = []
     pivot = Entity(position = (0,10,0))
